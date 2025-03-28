8ef213fa385246918c7a0a79b1e9bfef716c0240
hassan <tosinibikunle1@gmail.com>
2025-03-28 08:02:23 +0100

    square root

---
+++ sqrt.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    if (x < 0) { return null; }
+    if (x < 2) { return x; }
+    let guess = x;
+    while (Math.abs(guess * guess - x) > 0.5) {
+        guess = (guess + x / guess) / 2;
+    }
+    return parseInt(guess, 10);    
+};
