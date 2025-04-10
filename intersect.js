cf9beb225caab7482305100ddad7eadd174602f2
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-10 14:47:03 +0000

    intersect

---
+++ intersect.js
@@ -0,0 +1,17 @@
+function intersect(nums1, nums2) {
+      const map = {};
+        const result = [];
+
+          for (let num of nums1) {
+              map[num] = (map[num] || 0) + 1;
+                }
+
+                  for (let num of nums2) {
+                      if (map[num] > 0) {
+                            result.push(num);
+                                  map[num]--;
+                                      }
+                                        }
+
+                                          return result;
+                                          }
