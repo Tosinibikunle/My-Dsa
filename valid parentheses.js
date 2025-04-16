82817fb239a659bb19871f9377c4961ab49cf7fb
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-04-16 17:47:36 +0000

    valid parentheses

---
+++ valid parentheses.js
@@ -0,0 +1,8 @@
+function isValid(s) {
+      const stack = [], map = { '(': ')', '[': ']', '{': '}' };
+        for (let char of s) {
+            if (map[char]) stack.push(map[char]);
+                else if (char !== stack.pop()) return false;
+                  }
+                    return stack.length === 0;
+                    }
