5e0c82b62594c4b82b0f20f7808de3a0d84b7ab8
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-02 12:05:12 +0000

    tree t Graph

---
+++ treeToGraph.js
@@ -0,0 +1,15 @@
+function treeToGraph(node, graph = {}, parent = null) {
+      if (!node) return graph;
+        if (!graph[node.val]) graph[node.val] = [];
+
+          if (parent) {
+              graph[node.val].push(parent.val);
+                  graph[parent.val].push(node.val);
+                    }
+
+                      for (let child of node.children || []) {
+                          treeToGraph(child, graph, node);
+                            }
+
+                              return graph;
+                              }
