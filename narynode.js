199c1d63eec1072494488c084dd05e60fb21e74b
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-02 12:06:59 +0000

    nary node

---
+++ narynode.js
@@ -0,0 +1,14 @@
+class NaryNode {
+      constructor(val) {
+          this.val = val;
+              this.children = [];
+                }
+                }
+
+                function traverseNary(node) {
+                  if (!node) return;
+                    console.log(node.val);
+                      for (let child of node.children) {
+                          traverseNary(child);
+                            }
+                            }
