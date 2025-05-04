ab1240111e480a31a3d066b505dc55fe610454d2
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-04 17:42:31 +0000

    narynode minor correction

--- narynode.js
+++ narynode.js
@@ -1,14 +1,14 @@
 class NaryNode {
-      constructor(val) {
-          this.val = val;
-              this.children = [];
+ constructor(val) {
+  this.val = val;
+  this.children = [];
                 }
                 }
 
-                function traverseNary(node) {
-                  if (!node) return;
-                    console.log(node.val);
-                      for (let child of node.children) {
-                          traverseNary(child);
+function traverseNary(node) {
+  if (!node) return;
+  console.log(node.val);
+  for (let child of node.children) {
+     traverseNary(child);
                             }
                             }
