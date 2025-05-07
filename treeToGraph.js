111cbc3b79401f88e47cede7390e89789684b4eb
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-07 21:14:43 +0000

    mod tree to graph

--- treeToGraph.js
+++ treeToGraph.js
@@ -1,15 +1,15 @@
 function treeToGraph(node, graph = {}, parent = null) {
       if (!node) return graph;
-        if (!graph[node.val]) graph[node.val] = [];
+      if (!graph[node.val]) graph[node.val] = [];
 
-          if (parent) {
-              graph[node.val].push(parent.val);
-                  graph[parent.val].push(node.val);
-                    }
+      if (parent) {
+         graph[node.val].push(parent.val);
+         graph[parent.val].push(node.val);
+          }
 
-                      for (let child of node.children || []) {
-                          treeToGraph(child, graph, node);
-                            }
+      for (let child of node.children || []) {
+        treeToGraph(child, graph, node);
+           }
 
-                              return graph;
-                              }
+         return graph;
+                 }
