a29df179a1aa4a457e83a29b69bde80271e20365
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-07 21:09:05 +0000

    mod segment

--- segmenttree.js
+++ segmenttree.js
@@ -1,26 +1,27 @@
 class SegmentTree {
       constructor(arr) {
           this.n = arr.length;
-              this.tree = new Array(4 * this.n).fill(0);
-                  this.build(arr, 0, 0, this.n - 1);
+          this.tree = new Array(4 * this.n).fill(0);
+          this.build(arr, 0, 0, this.n - 1);
                     }
 
-                      build(arr, node, start, end) {
-                          if (start === end) {
-                                this.tree[node] = arr[start];
-                                    } else {
-                                          const mid = Math.floor((start + end) / 2);
-                                                this.build(arr, 2 * node + 1, start, mid);
-                                                      this.build(arr, 2 * node + 2, mid + 1, end);
-                                                            this.tree[node] = this.tree[2 * node + 1] + this.tree[2 * node + 2];
-                                                                }
-                                                                  }
+                     
+      build(arr, node, start, end) {
+          if (start === end) {
+            this.tree[node] = arr[start];
+            } else {
+          const mid = Math.floor((start + end) / 2);
+          this.build(arr, 2 * node + 1, start, mid);
+          this.build(arr, 2 * node + 2, mid + 1, end);
+          this.tree[node] = this.tree[2 * node + 1] + this.tree[2 * node + 2];
+                                  }
+                                      }
 
-                                                                    query(node, start, end, l, r) {
-                                                                        if (r < start || end < l) return 0;
-                                                                            if (l <= start && end <= r) return this.tree[node];
-                                                                                const mid = Math.floor((start + end) / 2);
-                                                                                    return this.query(2 * node + 1, start, mid, l, r) +
+          query(node, start, end, l, r) {
+             if (r < start || end < l) return 0;
+             if (l <= start && end <= r) return this.tree[node];
+             const mid = Math.floor((start + end) / 2);
+             return this.query(2 * node + 1, start, mid, l, r) +
                                                                                                this.query(2 * node + 2, mid + 1, end, l, r);
                                                                                                  }
 
