fc40c797ceacb073f45033670988ebecf607e1e5
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-09 16:59:23 +0000

    mod threaded node

--- threadedNode.js
+++ threadedNode.js
@@ -1,8 +1,8 @@
 class ThreadedNode {
       constructor(val) {
           this.val = val;
-              this.left = null;
-                  this.right = null;
-                      this.isThreaded = false; // if right is a thread to inorder successor
-                        }
+          this.left = null;
+          this.right = null;
+          this.isThreaded = false; // if right is a thread to inorder successor
+                    }
                         }
