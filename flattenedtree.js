8fc48fc6898c8c592c6d8dcca9984518e3f38d2a
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-09 16:57:49 +0000

    mod flatened tree

--- flattenedtree.js
+++ flattenedtree.js
@@ -1,17 +1,17 @@
 class FlattenedTree {
       constructor(n) {
           this.n = n;
-              this.adj = Array.from({ length: n }, () => []);
-                  this.flat = [];
-                      this.inTime = Array(n).fill(0);
-                          this.outTime = Array(n).fill(0);
-                              this.timer = 0;
-                                }
+          this.adj = Array.from({ length: n }, () => []);
+          this.flat = [];
+          this.inTime = Array(n).fill(0);
+          this.outTime = Array(n).fill(0);
+          this.timer = 0;
+            }
 
-                                  addEdge(u, v) {
-                                      this.adj[u].push(v);
-                                          this.adj[v].push(u);
-                                            }
+     addEdge(u, v) {
+        this.adj[u].push(v);
+        this.adj[v].push(u);
+                }
 
                                               dfs(node, parent) {
                                                   this.inTime[node] = this.timer++;
