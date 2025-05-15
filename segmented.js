e60d5e860a48eeaf515223b55004fdaaaf14f20c
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-15 20:47:01 +0000

    further segmented mod

--- segmented.js
+++ segmented.js
@@ -16,23 +16,23 @@ class SegmentTree {
                }
                        }
 
-                                                                    query(node, l, r, ql, qr) {
-                                                                        if (qr < l || ql > r) return 0;
-                                                                            if (ql <= l && r <= qr) return this.tree[node];
-                                                                                const mid = Math.floor((l + r) / 2);
-                                                                                    return this.query(2 * node + 1, l, mid, ql, qr) + this.query(2 * node + 2, mid + 1, r, ql, qr);
-                                                                                      }
+      query(node, l, r, ql, qr) {
+          if (qr < l || ql > r) return 0;
+          if (ql <= l && r <= qr) return this.tree[node];
+          const mid = Math.floor((l + r) / 2);
+         return this.query(2 * node + 1, l, mid, ql, qr) + this.query(2 * node + 2, mid + 1, r, ql, qr);
+                        }
 
-                                                                                        update(node, l, r, idx, val) {
-                                                                                            if (l === r) {
-                                                                                                  this.tree[node] = val;
-                                                                                                      } else {
-                                                                                                            const mid = Math.floor((l + r) / 2);
-                                                                                                                  if (idx <= mid) this.update(2 * node + 1, l, mid, idx, val);
-                                                                                                                        else this.update(2 * node + 2, mid + 1, r, idx, val);
-                                                                                                                              this.tree[node] = this.tree[2 * node + 1] + this.tree[2 * node + 2];
-                                                                                                                                  }
-                                                                                                                                    }
+      update(node, l, r, idx, val) {
+           if (l === r) {
+              this.tree[node] = val;
+                } else {
+                    const mid = Math.floor((l + r) / 2);
+                    if (idx <= mid) this.update(2 * node + 1, l, mid, idx, val);
+                    else this.update(2 * node + 2, mid + 1, r, idx, val);
+                    this.tree[node] = this.tree[2 * node + 1] + this.tree[2 * node + 2];
+                                                  }
+                                      }
 
                                                                                                                                       rangeQuery(ql, qr) {
                                                                                                                                           return this.query(0, 0, this.n - 1, ql, qr);
