c2a53407d1e6e38d29c69cf4795a0cc56d4872d1
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-18 21:24:23 +0000

    mod hash

--- hashTable.js
+++ hashTable.js
@@ -1,17 +1,17 @@
 class HashTable {
       constructor(size = 42) {
           this.buckets = new Array(size);
+    }
+
+      hash(key) {
+        return key.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0) % this.buckets.length;
+           }
+
+      set(key, value) {
+        const index = this.hash(key);
+        if (!this.buckets[index]) {
+           this.buckets[index] = [];
             }
-
-              hash(key) {
-                  return key.split('').reduce((acc, char) => acc + char.charCodeAt(0), 0) % this.buckets.length;
-                    }
-
-                      set(key, value) {
-                          const index = this.hash(key);
-                              if (!this.buckets[index]) {
-                                    this.buckets[index] = [];
-                                        }
                                             this.buckets[index].push([key, value]);
                                               }
 
