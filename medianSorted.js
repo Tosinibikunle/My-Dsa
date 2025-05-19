ad605f060d7420ca2f8602b475e23f7a82b7d064
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-19 15:51:48 +0000

    mod median sorted

--- medianSorted.js
+++ medianSorted.js
@@ -1,8 +1,8 @@
 var findMedianSortedArrays = function(nums1, nums2) {
-      var allNums = nums1.concat(nums2);
-        allNums = allNums.sort((a, b) => a - b)
-          var middleIndex = Math.floor(allNums.length / 2);
+     var allNums = nums1.concat(nums2);
+     allNums = allNums.sort((a, b) => a - b)
+     var middleIndex = Math.floor(allNums.length / 2);
 
-            if (allNums.length % 2 === 1) return allNums[middleIndex];
+          if (allNums.length % 2 === 1) return allNums[middleIndex];
               else return ((allNums[middleIndex - 1] + allNums[middleIndex]) / 2)
               };
