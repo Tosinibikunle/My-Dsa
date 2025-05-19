25e1bf85376a170249896d626d529b876fc6da83
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-19 15:53:18 +0000

    final median mod

--- medianSorted.js
+++ medianSorted.js
@@ -3,6 +3,7 @@ var findMedianSortedArrays = function(nums1, nums2) {
      allNums = allNums.sort((a, b) => a - b)
      var middleIndex = Math.floor(allNums.length / 2);
 
-          if (allNums.length % 2 === 1) return allNums[middleIndex];
+     if (allNums.length % 2 === 1) return allNums[middleIndex];
               else return ((allNums[middleIndex - 1] + allNums[middleIndex]) / 2)
-              };
+            
+};
