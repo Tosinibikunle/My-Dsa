a6122a461f4757b7c2f1f2ae4747054003c72867
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-21 13:33:09 +0000

    mod minheap

--- minheap.js
+++ minheap.js
@@ -1,20 +1,20 @@
 class MinHeap {
       constructor() {
           this.heap = [];
+   }
+
+      getParentIndex(i) { return Math.floor((i - 1) / 2); }
+      getLeftChildIndex(i) { return 2 * i + 1; }
+      getRightChildIndex(i) { return 2 * i + 2; }
+
+      swap(i, j) {
+         [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
+          }
+
+      insert(value) {
+          this.heap.push(value);
+          this.heapifyUp();
             }
-
-              getParentIndex(i) { return Math.floor((i - 1) / 2); }
-                getLeftChildIndex(i) { return 2 * i + 1; }
-                  getRightChildIndex(i) { return 2 * i + 2; }
-
-                    swap(i, j) {
-                        [this.heap[i], this.heap[j]] = [this.heap[j], this.heap[i]];
-                          }
-
-                            insert(value) {
-                                this.heap.push(value);
-                                    this.heapifyUp();
-                                      }
 
                                         heapifyUp() {
                                             let index = this.heap.length - 1;
