40e0a28549fd30015519ce3587c45b779d53d232
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-22 16:58:44 +0000

    new minheap mod

--- minheap.js
+++ minheap.js
@@ -28,19 +28,19 @@ class MinHeap {
             }
 
       extractMin() {
-                                                                                        if (this.heap.length === 0) return null;
-                                                                                            if (this.heap.length === 1) return this.heap.pop();
+          if (this.heap.length === 0) return null;
+          if (this.heap.length === 1) return this.heap.pop();
 
-                                                                                                const min = this.heap[0];
-                                                                                                    this.heap[0] = this.heap.pop();
-                                                                                                        this.heapifyDown();
-                                                                                                            return min;
-                                                                                                              }
+          const min = this.heap[0];
+          this.heap[0] = this.heap.pop();
+          this.heapifyDown();
+          return min;
+       }
 
-                                                                                                                heapifyDown() {
-                                                                                                                    let index = 0;
-                                                                                                                        while (this.getLeftChildIndex(index) < this.heap.length) {
-                                                                                                                              let smallerChildIndex = this.getLeftChildIndex(index);
+      heapifyDown() {
+          let index = 0;
+          while (this.getLeftChildIndex(index) < this.heap.length) {
+             let smallerChildIndex = this.getLeftChildIndex(index);
                                                                                                                                     const rightChildIndex = this.getRightChildIndex(index);
 
                                                                                                                                           if (
