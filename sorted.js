a3ce78e5278db5c784594370066f333efedab2dc
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-27 19:58:07 +0000

    sorted

---
+++ sorted.js
@@ -0,0 +1,15 @@
+class SortedArray {
+      constructor() {
+          this.arr = [];
+            }
+
+              insert(val) {
+                  this.arr.push(val);
+                      this.arr.sort((a, b) => a - b);
+                        }
+
+                          extractMin() {
+                              return this.arr.shift(); // Removes first (smallest)
+                                }
+                                }
+}
