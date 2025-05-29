c1a04297999fc39a2c9873c1331ae3857bcfb4ab
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-29 19:26:24 +0000

    lifostackmod

--- lifostack.js
+++ lifostack.js
@@ -1,18 +1,17 @@
 class Stack {
       constructor() {
-          this.items = [];
-            }
+         this.items = [];
+    }
 
-              push(item) {
-                  this.items.push(item);
-                    }
+    push(item) {
+      this.items.push(item);
+   }
 
-                      pop() {
-                          return this.items.pop();
-                            }
+   pop() {
+     return this.items.pop();
+     }
 
-                              peek() {
-                                  return this.items[this.items.length - 1];
-                                    }
-                                    }
-}
+   peek() {
+     return this.items[this.items.length - 1];
+         }
+    }
