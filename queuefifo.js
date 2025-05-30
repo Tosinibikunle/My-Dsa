623a020b8455f036c155290f7d8c0fceb9ef8847
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-05-30 13:51:32 +0000

    queuefifo

---
+++ queuefifo.js
@@ -0,0 +1,17 @@
+class Queue {
+      constructor() {
+          this.items = [];
+            }
+
+              enqueue(item) {
+                  this.items.push(item);
+                    }
+
+                      dequeue() {
+                          return this.items.shift();
+                            }
+
+                              peek() {
+                                  return this.items[0];
+                                    }
+                                    }
