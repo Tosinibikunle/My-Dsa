37462ea34e383a1f790745c2019c375f6036d508
Tosinibikunle <tosinibikunle1@gmail.com>
2025-07-30 11:25:37 +0100

    tstnode toclipboard etc formatted

--- toClipboard.js
+++ toClipboard.js
@@ -1,8 +1,8 @@
 function copyToClipboard(text) {
-      return navigator.clipboard.writeText(text);
-      }
+  return navigator.clipboard.writeText(text);
+}
 
-      // Example:
-      copyToClipboard('Hello World!')
-        .then(() => console.log('Copied!'))
-          .catch(err => console.error('Failed to copy:', err));
+// Example:
+copyToClipboard('Hello World!')
+  .then(() => console.log('Copied!'))
+  .catch(err => console.error('Failed to copy:', err));
