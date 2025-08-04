f509c5623e7f5b5897aee749601d959111abfda9
Tosinibikunle <tosinibikunle1@gmail.com>
2025-08-04 16:54:20 +0100

    flatmap, flattenedtree format

--- flatmap.js
+++ flatmap.js
@@ -1,7 +1,7 @@
 function flatMapArray(arr, callback) {
       return arr.flatMap(callback);
-      }
+}
 
-      // Example:
-      console.log(flatMapArray([1, 2, 3], x => [x, x * 2]));
-      // [1, 2, 2, 4, 3, 6]
+// Example:
+console.log(flatMapArray([1, 2, 3], x => [x, x * 2]));
+// [1, 2, 2, 4, 3, 6]
