a5e68c2d84ae3ac3f59e6ac6970e36f0b03f9690
Tosin Ibikunle <tosinibikunle1@gmail.com>
2025-11-07 15:24:39 +0100

    swap nodes in pairs

---
+++ swap-nodes.py
@@ -0,0 +1,7 @@
+def swapPairs(self, head):
+    if head and head.next:
+        tmp = head.next
+        head.next = self.swapPairs(tmp.next)
+        tmp.next = head
+        return tmp
+    return head
