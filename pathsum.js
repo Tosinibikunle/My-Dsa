257e0034fac9b4f07090fe15c0c9767cf761b8ab
TosinIbikunle <tosinibikunle1@gmail.com>
2025-11-08 11:15:00 +0100

    partition list

--- pathsum.js
+++ pathsum.js
@@ -1,15 +1,8 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @param {number} sum
- * @return {boolean}
- */
+
+//  * @param {TreeNode} root
+//  * @param {number} sum
+//  * @return {boolean}
+//  */
 const hasPathSum = function (root, sum) {
     if (root === null)
         return false
