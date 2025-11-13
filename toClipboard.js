98b42673e000ad3037e8be524234f3be525da06a
TosinIbikunle <tosinibikunle1@gmail.com>
2025-11-14 00:04:44 +0100

    removed comment

--- toClipboard.js
+++ toClipboard.js
@@ -2,7 +2,7 @@ function copyToClipboard(text) {
   return navigator.clipboard.writeText(text);
 }
 
-// Example:
+
 copyToClipboard('Hello World!')
   .then(() => console.log('Copied!'))
   .catch(err => console.error('Failed to copy:', err));
