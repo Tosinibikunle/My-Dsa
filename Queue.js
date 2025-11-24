8b79e980f08bf10b2d8cae0fdeb3c0bd976a05ee
TosinIbikunle <tosinibikunle1@gmail.com>
2025-11-24 22:59:54 +0100

    queue modified

--- Queue.js
+++ Queue.js
@@ -1,25 +1,25 @@
 class Queue {
-      constructor() {
-          this.items = [];
-            }
+  constructor() {
+    this.items = [];
+  }
 
-      enqueue(element) {
-          this.items.push(element);
-            }
+  enqueue(element) {
+    this.items.push(element);
+  }
 
-      dequeue() {
-          return this.items.shift();
-       }
+  dequeue() {
+    return this.items.shift();
+  }
 
-      front() {
-          return this.items[0];
-            }
+  front() {
+    return this.items[0];
+  }
 
-      isEmpty() {
-          return this.items.length === 0;
-        }
+  isEmpty() {
+    return this.items.length === 0;
+  }
 
-      size() {
-         return this.items.length;
-            }
-               }
+  size() {
+    return this.items.length;
+  }
+}
