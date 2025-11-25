5127acd5f0a6ea990c24186b8f1fbb0f802903cf
TosinIbikunle <tosinibikunle1@gmail.com>
2025-11-25 07:16:08 +0100

    modified threadedNode and tstnode

--- threadedNode.js
+++ threadedNode.js
@@ -1,8 +1,11 @@
 class ThreadedNode {
+
     constructor(val) {
+
         this.val = val;
         this.left = null;
         this.right = null;
         this.isThreaded = false;
+        
     }
 }
