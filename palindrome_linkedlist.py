86b0669f7cae66212131f41d94b2142fd3d6f397
TosinIbikunle <tosinibikunle1@gmail.com>
2025-11-26 20:49:25 +0100

    matrix_sum_divisble_by_k

--- palindrome_linkedlist.py
+++ palindrome_linkedlist.py
@@ -1,8 +1,5 @@
 
-# class ListNode(object):
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+
 class Solution(object):
     def isPalindrome(self, head):
         l = []
