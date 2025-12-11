f6ef73e825a74c6fc7cceb47dba3ec4d57e0b4d6
TosinIbikunle <tosinibikunle1@gmail.com>
2025-12-11 22:08:06 +0100

    formatted deep deepClone and count_covered_building

--- count_covered_building.py
+++ count_covered_building.py
@@ -1,17 +1,17 @@
 class Solution:
     def countCoveredBuildings(self, n, buildings):
-        xMax, yMax=[0]*(n+1), [0]*(n+1)
-        xMin, yMin=[1<<31]*(n+1), [1<<31]*(n+1)
+        xMax, yMax = [0] * (n + 1), [0] * (n + 1)
+        xMin, yMin = [1 << 31] * (n + 1), [1 << 31] * (n + 1)
 
         for x, y in buildings:
-            xMin[y]=min(xMin[y], x)
-            xMax[y]=max(xMax[y], x)
-            yMin[x]=min(yMin[x], y)
-            yMax[x]=max(yMax[x], y)
+            xMin[y] = min(xMin[y], x)
+            xMax[y] = max(xMax[y], x)
+            yMin[x] = min(yMin[x], y)
+            yMax[x] = max(yMax[x], y)
 
-        cnt=0
+        cnt = 0
         for x, y in buildings:
-            coverX=(xMin[y]<x & x<xMax[y])
-            coverY=(yMin[x]<y & y<yMax[x])
-            cnt+=(coverX & coverY)
+            coverX = xMin[y] < x & x < xMax[y]
+            coverY = yMin[x] < y & y < yMax[x]
+            cnt += coverX & coverY
         return cnt
--- deepClone.js
+++ deepClone.js
@@ -1,10 +1,10 @@
 function deepClone(obj) {
 
       return structuredClone(obj);
-      
+
 }
 
-      const original = { a: 1, b: new Map([['key', 'value']]) };
-      const clone = deepClone(original);
+const original = { a: 1, b: new Map([['key', 'value']]) };
+const clone = deepClone(original);
 
-      console.log(clone);
+console.log(clone);
