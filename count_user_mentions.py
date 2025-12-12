9241baacff4c240d0e80e2d10645f6d020c83680
TosinIbikunle <tosinibikunle1@gmail.com>
2025-12-12 22:53:48 +0100

    modified count_user_mentions

--- count_user_mentions.py
+++ count_user_mentions.py
@@ -1,9 +1,15 @@
+from typing import Counter,deque
 class Solution:
-    def countMentions(self, n: int, e: List[List[str]]) -> List[int]:
+    def countMentions(self, n, e):
+
         z,allMentioned,allUsers,off = Counter(),0,{*range(n)},deque()
+
         for tm,_,ids in sorted((int(tm),'M' in tp,ids) for tp,tm,ids in e):
+
             while off and off[-1][0]<=tm: off.pop()
+
             if ids=='ALL': allMentioned += 1
+            
             elif ids=='HERE': z.update(allUsers-{ide for _,ide in off})
             elif 'id' in ids: z.update(int(ide[2:]) for ide in ids.split())
             else: off.appendleft([tm+60,int(ids)])
