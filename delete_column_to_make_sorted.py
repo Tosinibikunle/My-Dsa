e48795c8ba0738dd163e96cdb9a079d0ea7c346c
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2025-12-20 18:55:51 +0100

    Remove type hints from method signatures

--- delete_column_to_make_sorted.py
+++ delete_column_to_make_sorted.py
@@ -1,11 +1,11 @@
 class Solution:
-    def isSortedColumn(self, col: int, strs: list[str]) -> bool:
+    def isSortedColumn(self, col, strs):
         for i in range(1, len(strs)):
             if strs[i][col] < strs[i - 1][col]:
                 return False
         return True
 
-    def minDeletionSize(self, strs: list[str]) -> int:
+    def minDeletionSize(self, strs):
         count = 0
         for i in range(len(strs[0])):
             if not self.isSortedColumn(i, strs):
