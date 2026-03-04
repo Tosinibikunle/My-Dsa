6b38892cdbf3565b3dfe8ceb7e7c4b6bf735b775
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-03-04 22:18:59 +0100

    Delete Linkedlist.py

--- Linkedlist.py
+++
@@ -1,9 +0,0 @@
-class ListNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.next = None
-
-
-class LinkedList:
-    def __init__(self, head=None):
-        self.head = head
