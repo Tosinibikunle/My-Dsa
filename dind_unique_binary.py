655745aa83676d3cedb66735ca7853baa44e5d1b
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-03-08 21:06:21 +0100

    Implement solution to find different binary string

---
+++ dind_unique_binary.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def findDifferentBinaryString(self, nums):
+        done = [int(num, 2) for num in nums]
+        n = len(nums)
+        for i in range(n+1):
+            if not i in done:
+                return format(i, '0'+str(n)+'b')
