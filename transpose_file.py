1d3f112cc3a254fa9f8cfd3d6786d6ae35269285
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-04-09 22:41:10 +0100

    Create transpose_file.py for transposing text
    Add a script to transpose lines of a text file.

---
+++ transpose_file.py
@@ -0,0 +1,10 @@
+python3 -c '
+from collections import defaultdict
+data = defaultdict(list)
+with open("file.txt") as f:
+   for line in f:
+       for i, word in enumerate(line.split()):
+           data[i].append(word)
+for line in data.values():
+    print(" ".join(line))
+'
