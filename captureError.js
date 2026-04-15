7089d52caf0fc9e7d0657613243aefebd884bad6
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-04-15 22:53:01 +0100

    Add stack trace capturing to CustomError

--- captureError.js
+++ captureError.js
@@ -1,10 +1,12 @@
 function captureErrorStack(errorObj, constructorOpt) {
+  
   Error.captureStackTrace(errorObj, constructorOpt);
 }
 
-// Example (Node.js):
 class CustomError {
+  
   constructor(message) {
+    
     this.message = message;
     captureErrorStack(this, CustomError);
   }
