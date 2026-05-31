45882cfc66aad8b154a1a1676e8cf0e58f7f500e
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-05-31 21:08:31 +0100

    Delete findLCA.js

--- findLCA.js
+++
@@ -1,6 +0,0 @@
-function findLCA(root, p, q) {
-  if (!root || root === p || root === q) return root;
-  const left = findLCA(root.left, p, q);
-  const right = findLCA(root.right, p, q);
-  return left && right ? root : left || right;
-}
