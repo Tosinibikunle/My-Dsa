ccd5e7894055c0dd1e0d3e9911f3c709ab17455c
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-06-07 22:44:03 +0100

    Add method to count covered buildings
    Implement countCoveredBuildings method to calculate covered buildings based on given coordinates.

--- count_covered_building.py
+++ count_covered_building.py
@@ -1,9 +1,12 @@
 class Solution:
+    
     def countCoveredBuildings(self, n, buildings):
+        
         xMax, yMax = [0] * (n + 1), [0] * (n + 1)
         xMin, yMin = [1 << 31] * (n + 1), [1 << 31] * (n + 1)
 
         for x, y in buildings:
+            
             xMin[y] = min(xMin[y], x)
             xMax[y] = max(xMax[y], x)
             yMin[x] = min(yMin[x], y)
@@ -11,7 +14,9 @@ class Solution:
 
         cnt = 0
         for x, y in buildings:
+            
             coverX = xMin[y] < x & x < xMax[y]
             coverY = yMin[x] < y & y < yMax[x]
             cnt += coverX & coverY
+            
         return cnt
