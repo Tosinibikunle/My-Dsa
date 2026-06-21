692294b635e98675e3d0f6b29944815ed90c66a8
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-06-21 20:49:53 +0100

    Implement maxIceCream function to calculate max bars

---
+++ max_ice_bars.py
@@ -0,0 +1,14 @@
+class Solution:
+    def maxIceCream(self, costs: List[int], coins: int) -> int:
+        costs.sort()
+
+        count = 0
+
+        for cost in costs:
+            if cost > coins:
+                break
+
+            count += 1
+            coins -= cost
+
+        return count
