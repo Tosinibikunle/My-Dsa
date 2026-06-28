b4be69c1ccd510e3c6c2fddb42a73cabeca33cfc
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-06-28 21:25:47 +0100

    Refactor specialTriplets method in Solution class

--- count_special_triplets.py
+++ count_special_triplets.py
@@ -1,5 +1,7 @@
 class Solution:
-    def specialTriplets(self, a: List[int]) -> int:
+    def specialTriplets(self, a):
+        
         z1,z2 = Counter(),Counter()
+        
         return sum((z2[v/2],z2.update({v:z1[v*2]}),z1.update([v]))[0]
             for v in a)%(10**9+7)
