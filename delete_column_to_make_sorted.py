1542c7e18a054a4c094706a435467e35a4da5b6d
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-07-03 22:33:13 +0100

    Implement column sorting check and deletion count

--- delete_column_to_make_sorted.py
+++ delete_column_to_make_sorted.py
@@ -1,13 +1,19 @@
 class Solution:
     def isSortedColumn(self, col, strs):
+        
         for i in range(1, len(strs)):
+            
             if strs[i][col] < strs[i - 1][col]:
+                
                 return False
         return True
 
     def minDeletionSize(self, strs):
+        
         count = 0
         for i in range(len(strs[0])):
+            
             if not self.isSortedColumn(i, strs):
+                
                 count += 1
         return count
