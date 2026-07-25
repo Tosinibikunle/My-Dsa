86041eda4f66384f2a574c2169f59a021a2cea97
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-07-25 22:12:33 +0100

    Delete grpanagrams.js

--- grpanagrams.js
+++
@@ -1,8 +0,0 @@
-function groupAnagrams(strs) {
-  const map = {};
-  for (let str of strs) {
-    const key = str.split("").sort().join("");
-    map[key] = (map[key] || []).concat(str);
-  }
-  return Object.values(map);
-}
