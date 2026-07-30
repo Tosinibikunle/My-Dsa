db2fe9c5a11d0c6e3580171c0968f32c5a93ebab
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-07-30 21:31:09 +0100

    Update find_people_with_secrets.py

--- find_people_with_secrets.py
+++ find_people_with_secrets.py
@@ -1,5 +1,6 @@
 class Solution(object):
     def findAllPeople(self, n, meetings, firstPerson):
+        
         meetings.sort(key=lambda x: x[2])
 
         parent = list(range(n))
@@ -7,34 +8,43 @@ class Solution(object):
         know[0] = know[firstPerson] = True
 
         def find(x):
+            
             if parent[x] != x:
+                
                 parent[x] = find(parent[x])
             return parent[x]
 
         def union(a, b):
+            
             pa, pb = find(a), find(b)
             if pa != pb:
                 parent[pb] = pa
 
         i = 0
         while i < len(meetings):
+            
             t = meetings[i][2]
             temp = []
 
             j = i
             while j < len(meetings) and meetings[j][2] == t:
+                
                 union(meetings[j][0], meetings[j][1])
                 temp.extend([meetings[j][0], meetings[j][1]])
                 j += 1
 
             for x in temp:
+                
                 if know[x]:
+                    
                     know[find(x)] = True
 
             for x in temp:
+                
                 know[x] |= know[find(x)]
 
             for x in temp:
+                
                 parent[x] = x
 
             i = j
