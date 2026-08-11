85115cd9ca83c9f0bf39c80d2c28a6fadfdeb775
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-08-11 17:08:40 +0100

    Refactor equal_sum_grid_partition.py by removing comments
    Removed comments for row and column sum computations, and total sum check.

--- equal_sum_grid_partition.py
+++ equal_sum_grid_partition.py
@@ -6,31 +6,26 @@ class Solution:
         m = len(grid)
         n = len(grid[0])
 
-        # Compute row sums
         for i in range(m):
             temp = 0
             for j in range(n):
                 temp += grid[i][j]
             rowSum.append(temp)
 
-        # Compute column sums
         for j in range(n):
             temp = 0
             for i in range(m):
                 temp += grid[i][j]
             colSum.append(temp)
 
-        # Total sum of grid
         totalSum = sum(sum(row) for row in grid)
 
-        # Check row partition
         currRowSum = 0
         for i in range(m):
             currRowSum += rowSum[i]
             if currRowSum == totalSum - currRowSum:
                 return True
 
-        # Check column partition
         currColSum = 0
         for i in range(n):
             currColSum += colSum[i]
