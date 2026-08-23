353c867fd5033724586293affa0e791c6f10f92d
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-08-23 20:49:59 +0100

    Update fancy_sequence.py

--- fancy_sequence.py
+++ fancy_sequence.py
@@ -6,20 +6,20 @@ class Fancy:
         self.add = 0
         self.arr = []
 
-    def append(self, val: int) -> None:
+    def append(self, val):
         inv = pow(self.mul, self.MOD - 2, self.MOD)
         stored = (val - self.add) % self.MOD
         stored = (stored * inv) % self.MOD
         self.arr.append(stored)
 
-    def addAll(self, inc: int) -> None:
+    def addAll(self, inc):
         self.add = (self.add + inc) % self.MOD
 
-    def multAll(self, m: int) -> None:
+    def multAll(self, m) ->:
         self.mul = (self.mul * m) % self.MOD
         self.add = (self.add * m) % self.MOD
 
-    def getIndex(self, idx: int) -> int:
+    def getIndex(self, idx):
         if idx >= len(self.arr):
             return -1
         return (self.arr[idx] * self.mul + self.add) % self.MOD
