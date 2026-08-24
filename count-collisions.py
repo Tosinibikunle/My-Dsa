22ee8e018ca4c3aff05f4764789b94daac52e326
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-08-24 22:44:07 +0100

    Delete count-collisions.py

--- count-collisions.py
+++
@@ -1,6 +0,0 @@
-class Solution:
-
-    def countCollisions(self, directions):
-        directions = directions.lstrip("L")
-        directions = directions.rstrip("R")
-        return directions.count("R") + directions.count("L")
