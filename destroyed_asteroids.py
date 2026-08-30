bea618cd9e73a3a7402d7750c7cfd69dfff94862
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-08-30 20:30:55 +0100

    Update destroyed_asteroids.py

--- destroyed_asteroids.py
+++ destroyed_asteroids.py
@@ -1,15 +1,13 @@
 class Solution:
-    def asteroidsDestroyed(self, mass: int, asteroids: List[int]) -> bool:
+    def asteroidsDestroyed(self, mass, asteroids):
         MAXA = 100000
         cnt = [0] * (MAXA + 1)
 
-        # Count frequency of each asteroid size
         for ast in asteroids:
             cnt[ast] += 1
 
         curmass = mass
 
-        # Process asteroids from smallest to largest
         for ast in range(1, MAXA + 1):
             while cnt[ast]:
                 if curmass < ast:
