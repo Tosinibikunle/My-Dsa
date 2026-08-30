c9278ae0b39e9d91f86258bee52fea97968cf4fc
Tosinibikunle <89074158+Tosinibikunle@users.noreply.github.com>
2026-08-30 20:32:35 +0100

    Update min-pen-for-shop.py

--- min-pen-for-shop.py
+++ min-pen-for-shop.py
@@ -1,5 +1,5 @@
 class Solution:
-    def bestClosingTime(self, customers: str) -> int:
+    def bestClosingTime(self, customers):
         y_rem = customers.count('Y')
         nn = 0
         
@@ -8,7 +8,7 @@ class Solution:
         min_i = 0
         
         for i, char in enumerate(customers, start=1):
-            # Evaluate current hour's customer
+        
             if char == 'Y':
                 y_rem -= 1
             else:
@@ -16,7 +16,7 @@ class Solution:
                 
             penalty = nn + y_rem
             
-            # Update minimum penalty and index
+            
             if penalty < min_p:
                 min_p = penalty
                 min_i = i
